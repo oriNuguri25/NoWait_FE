58d412ab841a98e1f8b803d5ef904379d7b5d605
hwang daeseong <klp2797@naver.com>
2025-06-20 17:11:29 +0900

    feat(user): 폴더 구조 및 라우팅 세팅

---
+++ apps/nowait-user/src/pages/order/AddMenuPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddMenuPage = () => {
+  return (
+    <div>AddMenuPage</div>
+  )
+}
+
+export default AddMenuPage
---
+++ apps/nowait-user/src/pages/order/RedirectToStorePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RedirectToStorePage = () => {
+  return (
+    <div>RedirectToStorePage</div>
+  )
+}
+
+export default RedirectToStorePage
