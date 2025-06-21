6487629048647fcff5b6314f71c0e8af5f851b3a
oriNuguri25 <byeongjun25@naver.com>
2025-06-21 22:29:15 +0900

    feat: admin, user favicon 추가

--- apps/nowait-admin/src/App.tsx
+++ apps/nowait-admin/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { Routes, Route } from "react-router";
 import AdminLayout from "./layout/AdminLayout";
 import AdminAnalytics from "./pages/AdminAnalytics/AdminAnalytics";
