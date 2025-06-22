85fbc8c0c49cc022d0afc23f58577c203003ed5d
unknown <klp2797@naver.com>
2025-06-22 23:04:13 +0900

    feat: 송금 페이지 계좌번호 복사 기능 추가

--- apps/nowait-user/src/pages/order/RemittanceRequestPage.tsx
+++ apps/nowait-user/src/pages/order/RemittanceRequestPage.tsx
@@ -3,6 +3,13 @@ import PageFooterButton from "../../components/order/PageFooterButton";
 import copy from "../../assets/icon/copy.svg";
 
 const RemittanceRequestPage = () => {
+  const account = "기업은행 611-000202-01-010";
+  const handleCopyClipBoard = async (account: string) => {
+    try {
+      await navigator.clipboard.writeText(account);
+      alert("복사가 완료되었습니다.")
+    } catch (e) {}
+  };
   return (
     <div className="flex min-h-full">
       <div className="flex-1 flex flex-col justify-center items-center">
@@ -20,9 +27,12 @@ const RemittanceRequestPage = () => {
           <h1 className="text-15-semibold text-navy-60">
             노웨잇대디자인과학생회
           </h1>
-          <button className="flex gap-1">
+          <button
+            onClick={() => handleCopyClipBoard(account)}
+            className="flex gap-1"
+          >
             <p className="inline text-16-medium underline text-navy-90">
-              기업은행 611-000202-01-010
+              {account}
             </p>
             <img src={copy} alt="복사 아이콘" />
           </button>
