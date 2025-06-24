43c4d0aa3f8d714edd0fccdbea6fcb846de95409
hwangdae <klp2797@naver.com>
2025-06-24 21:43:23 +0900

    style: order 페이지 전체 padding 변경

--- apps/nowait-user/src/components/common/SuccessMessagePage.tsx
+++ apps/nowait-user/src/components/common/SuccessMessagePage.tsx
@@ -20,16 +20,20 @@ const SuccessMessagePage = ({
 }: PropsType) => {
   return (
     <div className="flex min-h-full">
-      <div className="flex-1 flex flex-col justify-center items-center text-center">
-        <img
-          className="mb-2.5 w-[210px] h-[210px] bg-amber-300"
-          src={imageSrc}
-          alt={imageAlt}
-        />
-        <h1 className="text-headline-24-bold mb-2">{title}</h1>
-        <h2 className="whitespace-pre-line text-16-regular text-black-70">{message}</h2>
+      <div className="px-5">
+        <div className="flex-1 flex flex-col justify-center items-center text-center">
+          <img
+            className="mb-2.5 w-[210px] h-[210px] bg-amber-300"
+            src={imageSrc}
+            alt={imageAlt}
+          />
+          <h1 className="text-headline-24-bold mb-2">{title}</h1>
+          <h2 className="whitespace-pre-line text-16-regular text-black-70">
+            {message}
+          </h2>
+        </div>
+        <PageFooterButton onClick={onClick}>{buttonText}</PageFooterButton>
       </div>
-      <PageFooterButton onClick={onClick}>{buttonText}</PageFooterButton>
     </div>
   );
 };
