bf79abce671433d5adba233fcbfbdacf44829b5c
dgKim1 <kimdg3556@naver.com>
2025-07-29 19:41:53 +0900

    fix: 통계 페이지 api 연결

--- apps/nowait-admin/src/pages/AdminAnalytics/components/BoothSalesRankingCard .tsx
+++ apps/nowait-admin/src/pages/AdminAnalytics/components/BoothSalesRankingCard .tsx
@@ -8,6 +8,7 @@ interface BoothRanking {
   salesCount: number;
   rankChange: number;
   isCurrentBooth?: boolean;
+  profileImageUrl: string;
 }
 
 interface BoothSalesRankingCardProps {
@@ -47,7 +48,13 @@ const BoothSalesRankingCard: React.FC<BoothSalesRankingCardProps> = ({
                 <span className="text-14-semibold text-gray-600 w-[30px] h-[23px]">
                   {item.rank}
                 </span>
-                <div className="w-9 h-9 rounded-full bg-[#5A6ACF] mr-[10px]" />
+                <div className="w-9 h-9 rounded-full overflow-hidden mr-[10px]">
+                  <img
+                    src={item.profileImageUrl}
+                    alt={item.name}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
                 <div className="flex items-center text-sm gap-2">
                   <span className="text-16-semibold text-black">
                     {item.name}
