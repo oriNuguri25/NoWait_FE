310fe1073f1dad309c33b3f19f8fdbd5e6ed293a
oriNuguri25 <byeongjun25@naver.com>
2025-08-01 22:10:43 +0900

    feat: admin guides 페이지 UI 추가

--- apps/nowait-admin/src/components/AdminSidebar.tsx
+++ apps/nowait-admin/src/components/AdminSidebar.tsx
@@ -30,7 +30,7 @@ const AdminSidebar = () => {
   return (
     <aside
       className={`
-        h-screen flex flex-col justify-between bg-white fixed
+        h-screen flex flex-col justify-between bg-white fixed border-r border-r-[#ECECEC]
         ${
           isCompact
             ? "w-[60px] items-center px-[10px] py-5"
