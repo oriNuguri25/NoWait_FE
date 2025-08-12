53087c541a6cdbe9767608ca498a03afbdcd54e3
oriNuguri25 <byeongjun25@naver.com>
2025-08-12 16:50:08 +0900

    refactor: waitingIcon 수정

--- apps/nowait-user/src/pages/home/components/HomeIcon.tsx
+++ apps/nowait-user/src/pages/home/components/HomeIcon.tsx
@@ -4,7 +4,7 @@ interface WaitingIconProps {
 
 export function WaitingIcon({ waitingCount = 0 }: WaitingIconProps) {
   return (
-    <div className="px-1.5 py-1.5 rounded-md bg-[#FFF0EB]">
+    <div className="h-[21px] flex items-center px-1.5 pb-1.5 pt-1.25 rounded-md bg-[#FFF0EB]">
       <div className="font-bold text-[10px] text-[#FF5E07]">
         대기 {waitingCount}팀
       </div>
@@ -14,7 +14,7 @@ export function WaitingIcon({ waitingCount = 0 }: WaitingIconProps) {
 
 export function WaitingCardIcon({ waitingCount = 0 }: WaitingIconProps) {
   return (
-    <div className="px-1.5 py-1.5 w-13.5 h-5 rounded-md bg-[#FFFFFF]/30 flex items-center justify-center">
+    <div className="px-1.75 py-1.25 w-13.5 h-5 rounded-md bg-[#FFFFFF]/30 flex items-center justify-center">
       <div className="font-bold text-[10px] text-white">
         대기 {waitingCount}팀
       </div>
@@ -24,7 +24,7 @@ export function WaitingCardIcon({ waitingCount = 0 }: WaitingIconProps) {
 
 export function NotOpenIcon() {
   return (
-    <div className="px-1.5 py-1.5 rounded-md bg-[#F7F7F7]">
+    <div className="h-[21px] flex items-center px-1.5 pb-1.5 pt-1.25 rounded-md bg-[#F7F7F7]">
       <div className="font-bold text-[10px] text-[#AAAAAA]">오픈 전</div>
     </div>
   );
