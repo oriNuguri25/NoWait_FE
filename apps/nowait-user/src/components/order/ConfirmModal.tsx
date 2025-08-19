2330166a68376b53c4819b31458ae74fe06046dc
unknown <klp2797@naver.com>
2025-08-20 04:51:08 +0900

    feat: 모달 여백 수정 및 모달 오픈 시 스크롤 방지 추가

--- apps/nowait-user/src/components/order/ConfirmModal.tsx
+++ apps/nowait-user/src/components/order/ConfirmModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@repo/ui";
 import Portal from "../common/modal/Portal";
 import { AnimatePresence, motion } from "framer-motion";
@@ -25,6 +25,21 @@ const ConfirmModal = ({
   const handleClose = () => {
     setIsVisible(false);
   };
+  
+  // 모달 오픈 시 뒤 컨텐츠 스크롤 막기
+  useEffect(() => {
+    // fixed로 스크롤한 만큼의 상태로 뒤 컨텐츠 고정
+    const scrollY = window.scrollY;
+    document.body.style.position = "fixed";
+    document.body.style.top = `${scrollY}px`;
+
+    // 원래 위치 복원
+    return () => {
+      document.body.style.position = "";
+      document.body.style.top = "";
+      window.scrollTo(0, scrollY);
+    };
+  }, []);
 
   return (
     <Portal>
@@ -44,7 +59,7 @@ const ConfirmModal = ({
               exit={{ opacity: 0, y: "100%" }}
               onClick={(e) => e.stopPropagation()}
               transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="absolute left-1/2 bottom-8 -translate-x-1/2 w-[calc(100%-20px)] max-w-[430px] bg-white rounded-[20px] px-5 pt-9"
+              className="absolute left-1/2 bottom-8 -translate-x-1/2 w-[clamp(328px,calc(100%-32px),398px)] bg-white rounded-[20px] px-4 pt-9"
             >
               <h1 className="whitespace-pre-line text-headline-22-bold mb-2">
                 {title}
