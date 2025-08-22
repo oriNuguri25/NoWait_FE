3a3505707083a2cdbdce514d8e0a03648708f034
unknown <klp2797@naver.com>
2025-08-23 03:13:35 +0900

    feat: 주문 에러 확인 코드 생성

--- apps/nowait-user/src/pages/order/remittance/RemittancePage.tsx
+++ apps/nowait-user/src/pages/order/remittance/RemittancePage.tsx
@@ -21,7 +21,7 @@ const RemittancePage = () => {
   const { storeId } = useParams();
   const modal = useModal();
   const { cart } = useCartStore();
-  const [payer, setPayer] = useState("");
+  const [payer, setPayer] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string | null>("");
   const [remitValue, setRemitValue] = useState("");
   const totalPrice = sumTotalPrice(cart);
@@ -33,7 +33,7 @@ const RemittancePage = () => {
     enabled: !!storeId,
     select: (data) => data?.response,
   });
-  console.log(remittance)
+  console.log(remittance);
   // 기본 선택 값 지정하기
   useEffect(() => {
     if (!remittance) return;
