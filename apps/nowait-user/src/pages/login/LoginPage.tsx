d4c0904e7f35489628816754d94e3b1670975f98
oriNuguri25 <byeongjun25@naver.com>
2025-08-22 16:08:49 +0900

    refactor: 백그라운드 이미지 콘솔 추가 및 로그인 페이지 수정

--- apps/nowait-user/src/pages/login/LoginPage.tsx
+++ apps/nowait-user/src/pages/login/LoginPage.tsx
@@ -1,5 +1,8 @@
 import Logo from "../../assets/logo.svg?react";
 import LoginButton from "./LoginButton";
+import LoginImage_1 from "../../assets/login/login_img_1.png";
+import LoginImage_2 from "../../assets/login/login_img_2.svg?react";
+import LoginImage_3 from "../../assets/login/login_img_3.svg?react";
 
 const LoginPage = () => {
   return (
@@ -14,19 +17,19 @@ const LoginPage = () => {
           우리의 즐거운 축제
         </text>
       </div>
-      <div className="mt-32.5 px-3 flex justify-center">
-        <div className="w-75 aspect-square flex-shrink-0 bg-gray-200 border-2 border-dashed border-blue-400 flex items-center justify-center">
-          <span className="text-gray-500"></span>
+      <div className="mt-16 mx-[-24px] flex justify-center">
+        <div className="w-[432px] aspect-square flex-shrink-0 flex items-center justify-center">
+          <img src={LoginImage_1} alt="login_img_1" className="w-full" />
         </div>
       </div>
 
-      <div className="mt-22.5">
+      <div className="mt-20">
         <div className="flex justify-start">
-          <div className="bg-[#FFECE6] rounded-lg px-2.5 py-1.25 text-15-semibold text-primary h-8">
+          <div className="bg-[#FFECE6] rounded-lg px-2.5 py-1.25 text-14-semibold text-primary h-8">
             웨이팅
           </div>
         </div>
-        <div className="mt-3 text-start text-headline-28-bold text-black-100 justify-center">
+        <div className="mt-3 text-start text-headline-24-bold text-black-100 justify-center">
           줄 서지 않고
           <br />
           간편하게 입장해요
@@ -36,18 +39,18 @@ const LoginPage = () => {
           <br />
           순서가 되면 빠르게 입장할 수 있어요.
         </div>
-        <div className="mt-9.5 w-full h-52.5 bg-gray-200 justify-center items-center">
-          <span className="text-gray-500"></span>
+        <div className="mt-9.5 w-[325px] h-[210px] flex justify-center items-center">
+          <LoginImage_2 />
         </div>
       </div>
 
       <div className="mt-22.5">
         <div className="flex justify-end">
-          <div className="bg-[#FFECE6] rounded-lg px-2.5 py-1.25 text-15-semibold text-primary h-8 flex items-center justify-center">
+          <div className="bg-[#FFECE6] rounded-lg px-2.5 py-1.25 text-14-semibold text-primary h-8 flex items-center justify-center">
             주문
           </div>
         </div>
-        <div className="mt-3 text-end text-headline-28-bold text-black-100 justify-center">
+        <div className="mt-3 text-end text-headline-24-bold text-black-100 justify-center">
           앉은 자리에서 바로
           <br />
           주문하고 결제까지
@@ -57,18 +60,20 @@ const LoginPage = () => {
           <br />
           계좌번호가 안내되어 손쉽게 결제할 수 있어요.
         </div>
-        <div className="mt-9.5 w-full h-52.5 bg-gray-200 justify-center items-center">
-          <span className="text-gray-500"></span>
+        <div className="flex w-full justify-end items-center">
+          <div className="mt-9.5 w-[325px] h-[210px] flex justify-end items-center">
+            <LoginImage_3 />
+          </div>
         </div>
       </div>
 
       <div className="mt-22.5">
         <div className="flex justify-start">
-          <div className="bg-[#FFECE6] rounded-lg px-2.5 py-1.25 text-15-semibold text-primary h-8">
+          <div className="bg-[#FFECE6] rounded-lg px-2.5 py-1.25 text-14-semibold text-primary h-8">
             축제 앱
           </div>
         </div>
-        <div className="mt-3 text-start text-headline-28-bold text-black-100 justify-center">
+        <div className="mt-3 text-start text-headline-24-bold text-black-100 justify-center">
           축제 부스를
           <br />
           한눈에!
