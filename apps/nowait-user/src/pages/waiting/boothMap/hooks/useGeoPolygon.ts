0a5c32b728bd133da7c3afee1ae32413ee164c9d
unknown <klp2797@naver.com>
2026-02-01 18:18:31 +0900

    feat: 대학교 geojson 파일 모듈 스코프 캐싱

--- apps/nowait-user/src/pages/waiting/boothMap/hooks/useGeoPolygon.ts
+++ apps/nowait-user/src/pages/waiting/boothMap/hooks/useGeoPolygon.ts
@@ -1,32 +1,36 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+let cachedPaths: { lat: number; lng: number }[][] | null = null;
+
 export const useGeoPolygon = () => {
   const [paths, setPaths] = useState<{ lat: number; lng: number }[][]>([]);
 
   useEffect(() => {
+    if (cachedPaths) {
+      setPaths(cachedPaths);
+      return;
+    }
+
     const fetchPolygons = async () => {
       try {
         const { data } = await axios.get("/geojson/university.geojson");
 
-        // 좌표 변환
-        const coords = data.features.map((feature: any) => {
-          return feature.geometry.coordinates[0].map(
-            ([lng, lat]: [number, number]) => ({
-              lat,
-              lng,
-            })
-          );
-        });
+        const coords = data.features.map((feature: any) =>
+          feature.geometry.coordinates[0].map(
+            ([lng, lat]: [number, number]) => ({ lat, lng })
+          )
+        );
 
-        if (coords.length > 0) {
-          setPaths(coords); // [외곽, 구멍]
-        }
+        cachedPaths = coords;
+        setPaths(coords);
       } catch (err) {
         console.error("GeoJSON에 오류가 발생 했습니다.:", err);
       }
     };
+
     fetchPolygons();
   }, []);
+
   return paths;
 };
